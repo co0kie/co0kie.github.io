40ddd177ddb36610d15b8b120654e7e67ecaa867
Jech <j.pontoy@arcanys.com>
2019-11-07 09:02:29 +0700

    ngoi2x

---
+++ pages/test.js
@@ -0,0 +1,15 @@
+import Layout from '../components/layout'
+import Header from '../components/header'
+import Aside from '../components/aside'
+
+export default () => (
+  <Layout title='Jech - Web Developer | About me'>
+    <main className='🐸_ Wrapper _🐱'>
+      <Header />
+      <section className="_ Section Inner _">
+        <script data-ad-client="ca-pub-0610053756453486" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      </section>
+      <Aside />
+    </main>
+  </Layout>
+)
