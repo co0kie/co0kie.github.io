a7f5f0fee5a75389dcf123087cf127c85e9e9f8e
Jech <j3re.ch0@gmail.com>
2022-10-19 23:19:26 +0800

    fix

--- src/swup/app.js
+++ src/swup/app.js
@@ -2,11 +2,9 @@ import Swup from "swup";
 import SwupA11yPlugin from "@swup/a11y-plugin";
 import SwupHeadPlugin from "@swup/head-plugin";
 import SwupSlideTheme from "@swup/slide-theme";
-console.log("test1");
+
 document.addEventListener("DOMContentLoaded", () => {
-  // this event runs for every page view after initial load
   setTimeout(function () {
-    console.log("test2");
     const options = { cache: true };
     new Swup({
       plugins: [
@@ -17,4 +15,11 @@ document.addEventListener("DOMContentLoaded", () => {
       options,
     });
   }, 300);
+});
+
+document.addEventListener("swup:contentReplaced", () => {
+const c = document.querySelector(".main-container");
+  setTimeout(function () {
+    c.style.opacity = "1";
+  }, 500);
 });
